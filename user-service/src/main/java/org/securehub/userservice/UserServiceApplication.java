ebf55a028e83d349fc3bd821bc35979e21dd55f6
Vinula Senarathne <vinulapc@gmail.com>
2026-05-12 23:55:17 +0530

    feat(user): add initial user service and postgres server

---
+++ user-service/src/main/java/org/securehub/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package org.securehub.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+}
