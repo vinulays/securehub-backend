4137a288af66ed0db8d7c6d493ab6a4e4a8e8586
Vinula Senarathne <vinulapc@gmail.com>
2026-06-05 20:31:05 +0530

    feat(auth): add endpoint to get current user details

--- auth-service/src/main/java/org/securehub/authservice/config/WebClientConfig.java
+++ auth-service/src/main/java/org/securehub/authservice/config/WebClientConfig.java
@@ -1,15 +1,26 @@
 package org.securehub.authservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
 
+    @Value("${keycloak.cert-url}")
+    private String certUrl;
+
     @Bean
-    public WebClient webClient(){
+    public WebClient webClient() {
         return WebClient.builder().build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withJwkSetUri(certUrl).build();
     }
 
 }
