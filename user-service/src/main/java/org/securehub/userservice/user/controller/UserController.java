14ccf78f59fd377432a8a9b77ee2823aa36e99b0
Vinula Senarathne <vinulapc@gmail.com>
2026-06-07 13:49:35 +0530

    feat(gateway): add circuit breaker with time limiter

--- user-service/src/main/java/org/securehub/userservice/user/controller/UserController.java
+++ user-service/src/main/java/org/securehub/userservice/user/controller/UserController.java
@@ -4,15 +4,17 @@ import lombok.RequiredArgsConstructor;
 import org.securehub.userservice.security.model.AuthenticatedUser;
 import org.securehub.userservice.security.service.CurrentUserService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/users")
 public class UserController {
 
     private final CurrentUserService currentUserService;
 
-    @GetMapping("/api/users/me")
+    @GetMapping("/me")
     public AuthenticatedUser getCurrentUser(){
 
         return currentUserService.getCurrentUser();
